79bb2fb4acc3ea3984ac13a4eccf3e0d16d2fa3e
DominikaSzal <80176989+DominikaSzal@users.noreply.github.com>
2021-03-10 13:07:58 -0600

    Update milestone1.py

--- milestone1.py
+++ milestone1.py
@@ -63,6 +63,7 @@ def fibonacci_rabbits(n,k):
     
     return F2
 print(fibonacci_rabbits(5,3))
+
 def hamming_dist(dna1, dna2):
     count = 0
     d = 0
@@ -71,3 +72,36 @@ def hamming_dist(dna1, dna2):
             count += 1
         d += 1
     return count
+
+def rna2codon(triplet):
+    genetic_code = {
+        'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',        'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+        'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',        'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+
+        'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',        'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+        'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',        'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+
+        'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',        'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',        'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+
+        'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',        'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+        'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',        'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+    }
+    allowed_codons = set('ACGU')
+    tripletUP = triplet.upper()
+    for character in tripletUP:
+        if character not in allowed_codons:
+            return('Invalid')
+    return_val = genetic_code.get(tripletUP, 'Invalid')
+        
+  
+    
+    return return_val
+
+def rna2codons(rna):
+    amino_string = ''
+    for i in range(0,int(len(rna)/3)):
+        c = rna[3*i:3*i+3]
+        amino = rna2codon(c)
+        amino_string = amino_string + amino
+    return amino_string
