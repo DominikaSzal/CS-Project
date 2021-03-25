c8a76f98a001e2179659fd0f0f1ab88efa45fe90
Theroosterbrc <80182408+Theroosterbrc@users.noreply.github.com>
2021-03-25 18:24:08 -0500

    Update milestone2.py

--- milestone2.py
+++ milestone2.py
@@ -1,1 +1,16 @@
 
+def find_splice(dna_motif, dna):
+
+    start = 0
+    pos_list = []
+
+    for char in dna_motif:
+        position = dna.find(char,start)
+  
+
+        pos_list.append(position)
+  
+
+        start = position
+  
+    return pos_list
