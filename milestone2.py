0a73969af40b2cb2d9020793b27a13839191bd6f
Theroosterbrc <80182408+Theroosterbrc@users.noreply.github.com>
2021-04-15 18:38:07 -0500

    Update milestone2.py

--- milestone2.py
+++ milestone2.py
@@ -1,16 +1,20 @@
-
 def find_splice(dna_motif, dna):
 
     start = 0
     pos_list = []
 
-    for char in dna_motif:
+    for char in range( dna_motif):
+       {
         position = dna.find(char,start)
   
-
         pos_list.append(position)
-  
-
-        start = position
-  
+      
+        position = 0
+        while pos_list >15:
+            position =dna.find(char,15)
+            if char <=15 :
+                return pos_list
+        if dna_motif = False:
+            return[]
+         }   
     return pos_list
